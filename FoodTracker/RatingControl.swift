8ac3966e254b66158f9311bc6f425e707f2d6dde
murad <muradkv@gmail.com>
2019-07-26 20:41:11 +0300

    Add RatingControl class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by murad on 26/07/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
